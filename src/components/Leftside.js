3ef3feefbf2549683be13e30061103b558da83df
k-cicek <kevserrcicek@gmail.com>
2021-08-14 23:45:28 +0300

    Add & style content for left section

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -1,53 +1,138 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Leftside = (props) => {
- return (
-  <Container>
-   <ArtCard>
-    <UserInfo>
-     <CardBackground/>
-     <a>
-      <Photo/>
-      <Link>Welcome, there!</Link>
-     </a>
-     <a>
-      <AddAPhotoText>Add a photo</AddAPhotoText>
-     </a>
-    
-    </UserInfo>
-   </ArtCard>
-  </Container>
- )
-}
+  return (
+    <Container>
+      <ArtCard>
+        <UserInfo>
+          <CardBackground />
+          <a>
+            <Photo />
+            <Link>Welcome, there!</Link>
+          </a>
+          <a>
+            <AddAPhotoText>Add a photo</AddAPhotoText>
+          </a>
+        </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your network</span>
+            </div>
+            <img src="/images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" />
+          </span>
+        </Item>
+      </ArtCard>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-grid-area: leftside;
+  grid-area: leftside;
 `;
 
 const ArtCard = styled.div`
-text-align: center;
-overflow: hidden;
-margin-bottom: 8px;
-background-color: #fff;
-border-radius: 5px;
-transition: box-shadow 83ms;
-position: relative;
-border: none;
-box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
-
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  transition: box-shadow 83ms;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
 `;
 
-const UserInfo= styled.div`
-border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+const UserInfo = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding: 12px 12px 16px;
+  word-wrap: break-word;
+  word-break: break-word;
 `;
 
-const CardBackground = styled.div``;
+const CardBackground = styled.div`
+  background: url("/images/card-bg.svg");
+  background-position: center;
+  background-size: 462px;
+  height: 54px;
+  margin: -12px -12px 0;
+`;
 
-const Photo = styled.div``;
+const Photo = styled.div`
+  box-shadow: none;
+  background-image: url("/images/photo.svg");
+  width: 72px;
+  height: 72px;
+  box-sizing: border-box;
+  background-clip: content-box;
+  background-color: white;
+  background-position: center;
+  background-size: 60%;
+  background-repeat: no-repeat;
+  border: 2px solid white;
+  margin: -38px auto 12px;
+  border-radius: 50%;
+`;
 
-const Link = styled.div``;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
 
-const AddAPhotoText = styled.div``;
+const AddAPhotoText = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 400;
+`;
 
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
 
-export default Leftside
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.08);
+    }
+
+    div {
+     display: flex;
+     flex-direction: column;
+     text-align: left;
+     span {
+      font-size: 12px;
+      line-height: 1.333;
+      &:first-child{
+       color: rgba(0, 0, 0, 0.6);
+      }
+      &:nth-child(2) {
+       color: rgba(0, 0, 0, 1);
+      }
+     }
+    }
+  }
+
+  svg {
+   color: rgba(0, 0, 0, 1);
+  }
+`;
+
+const Item = styled.a``;
+
+export default Leftside;
